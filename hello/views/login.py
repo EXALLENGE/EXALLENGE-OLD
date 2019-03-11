c3f5541f7465f466d0e733984ef27a767564753e
Fedor <arbuzovfp@gmail.com>
2019-03-11 12:00:00 +0300

    fix

--- hello/views/login.py
+++ hello/views/login.py
@@ -3,24 +3,7 @@ from django.shortcuts import render, redirect
 from hello.models import User
 
 
-def login_decorator(func):
-    def _decorated(request, *args, **kwargs):
-        # если человек заходит на защещенные страницы, то нужно проверить куку и что-то сделать
-        if func.__name__ == 'login' or func.__name__ == 'registration':
-            # если кука то пусть идет на свой профиль, если нет то пусть идет на свою страницу
-            if check_user_cookie(request):
-                redirect('profile')
-            return func(request, *args, **kwargs)
-        else:
-            # если кука то пусть идет  хочет, если нет то пусть идет на страницу login
-            if check_user_cookie(request):
-                return func(request, *args, **kwargs)
-            redirect('login')
-    return _decorated
-
-
 def check_user_cookie(requst):
-
     pass
 
 
@@ -31,15 +14,18 @@ def get_or_none(classmodel, **kwargs):
         return None
 
 
-@login_decorator
 def login(request):
     if request.method == 'GET':
         return render(request, "login.html")
     elif request.method == 'POST':
-        pass
+        name, psw = request.POST['username'], request.POST['psw']
+        if get_or_none(User, name=name):
+            # поставить куку
+            # пустить в профиль
+            pass
+        return render(request, "login.html", {'message': 'У нас нет такого логина'})
 
 
-@login_decorator
 def registration(request):
     if request.method == 'GET':
         return render(request, "registration.html")
@@ -47,13 +33,13 @@ def registration(request):
         name, psw = request.POST['username'], request.POST['psw']
         if get_or_none(User, name=name):
             # такой mail уже есть
-            return render(request, "registration.html")
+            return render(request, "registration.html", {'message': 'Пользователь с таким email уже зарегестрирован'})
         # если такой email уже есть в базе сказать об этом
         # иначе отправить письмо с ссылкой
+        return render(request, "message.html", {'message': 'У нас нет такого логина'})
 
-@login_decorator
+
 def profile(request):
-    if request.method == 'GET':
         return render(request, "profile.html")
 
 
